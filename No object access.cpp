2313a9769fbcb9228ab7ca7ca7fcc6a038b6a307
NeilZhy <NeilZhy@163.com>
2017-04-05 11:34:18 +0800

    无对象调用函数，this指针

--- No object access.cpp
+++ No object access.cpp
@@ -7,7 +7,8 @@ class A
 public:
 	void Show()
 	{
-		cout << "Show()" << endl;
+		printf("%p",this);          //当我们外面的指针指向NULL的时候，这里打印的结果是0，我们知道了这里是通过this指针来调用函数的
+		//cout << "Show()  " << endl;
 		//cout << _a << endl;    如果加上这一句程序就会出错，因为这里我们访问了对象的内存空间，所以编译不过
 	}
 protected:
@@ -17,6 +18,7 @@ protected:
 
 int main()
 {
+	A a;
 	A* p = NULL;   
 	p->Show();    //无对象调用函数，这里实际上是传递一个地址，这里我们没有访问对象的空间，所以这里不会出错
 	return 0;
