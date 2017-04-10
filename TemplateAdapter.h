e4efd7a292f52ac661d922d9fd37bcf9fa75f496
NeilZhy <NeilZhy@163.com>
2017-04-10 08:53:32 +0800

    模板适配器头文件

--- TemplateAdapter.h
+++ TemplateAdapter.h
@@ -1,7 +1,7 @@
 #ifndef __TemplateAdapter_H__
 #define __TemplateAdapter_H__
 
-
+#include<iostream>
 #include<string>
 using namespace std;
 
@@ -129,7 +129,12 @@ void List<T>::PopFront()
 	}
 }
 
-template<typename T, typename Container>
+template<typename T, typename Container = List<T>>  //这里可以把这一条语句看成是一个抽象函数的形式，T和Container是两个参数
+										//所以这里可以给这两个参数缺省值，就是赋初值
+								//T可以理解成一个内置类型，如int，double等，Container可以是一个自定义的类，就是那个适配器
+								//这里的适配器就是链表或者顺序表
+								//这里我们在test函数中使用的时候，需要传递的是Queue<int, List<int>>这种形式，因为我们这里
+							//的Container也是在模板类的基础上面定义出来的，和下面的模板的模板参数比较一下
 class Stack
 {
 public:
@@ -188,6 +193,37 @@ void Queue<T, Container>::Show()
 {
 	_con.Show();
 }
+
+//template <typename T>
+//class SeqList
+//{
+//private:
+//	int _size;
+//	int _capacity;
+//	T* _data;
+//};
+//template<typename T, typename Container = List<T>>    //这条语句是原始的版本，和下面的模板的参数做比较
+//template <class T, template <class> class Container = SeqList > // 缺省参数，这个版本和上面的比较的话，就是修饰的类型
+				//多了一个template <class>，这样的话，整个类型就变了，所以我们的参数也需要变，参数变成了一个
+		//有模板的类型，所以我们这里就只需要填写顺序表，或者链表就好了
+		//这里的template <class> class可以理解成一个类型，就是比如我们在写函数的时候，参数有一个类型，而这里的类型就是
+					//一个模板的模板，就是说，这里的template <class>本身就是一个模板，然后又把它抽象为一个模板
+				//这里我们在使用的时候，就不需要像上面一样Queue<int, List<int>>，而只需要Stack<int> s;
+//class Stack
+//{
+//public:
+//	void Push(const T& x);
+//	void Pop();
+//	const T & Top();
+//	bool Empty();
+//private:
+//	Container<T > _con;
+//};
+//void Test()
+//{
+//	Stack<int > s1;
+//	Stack<int, SeqList> s2;
+//}
 
 
 
