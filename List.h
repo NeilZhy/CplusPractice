a42fa61e91f0cf6b69c03f25d3a58e9863fb2f11
NeilZhy <NeilZhy@163.com>
2017-05-02 11:56:17 +0800

    STLlist

--- List.h
+++ List.h
@@ -1,198 +1,118 @@
+#pragma once
 #include<iostream>
-
 using namespace std;
 
-typedef int Datatype;
+//void Show()
+//{
+//	cout << "hello" << endl;
+//}
 
+
+//链表节点
+template<class T>    //这里没有必要写template<class T，class ref，class ptr>了，因为链表节点不要这么东西  
 struct ListNode
 {
-	Datatype _data;
-	ListNode* _next;
-	ListNode(Datatype data)
+	ListNode<T>* _next;   //这里记住和之前说的，有模板类的地方就一定要想到写那个类，我开始的时候没有写这个
+	ListNode<T>* _prev;
+	T _data;
+public:
+	ListNode(T data = 0)
 		:_next(NULL)
+		, _prev(NULL)
 		, _data(data)
 	{}
 };
 
-class List
+//迭代器iterator
+template<class T,class Ref,class Ptr>
+//template<class T>
+class __iterator
 {
-	typedef ListNode Node;
+	typedef ListNode<T> Node;
+	typedef __iterator<T, Ref, Ptr> Self;
 public:
-	List()
-		:_head(NULL)
-		, _tail(NULL)
+	__iterator(Node* p)
+		:_ptr(p)
+	{}
+public:
+	bool operator!=(Node* p)
 	{
-
+		return *this != p;
 	}
 
-	List(const List& l)
-		:_head(NULL)
-		, _tail(NULL)
-	{
-		Node* cur = l._head;
-		while (cur)
-		{
-			this->PushBack(cur->_data);    //函数复用
-			cur = cur->_next;
-		}
-	}
 
-	~List()
-	{
-		Node* cur = _head;
-		Node* del = cur;
-		_head = _tail = NULL;
-		while (cur)
-		{
-			del = cur;
-			cur = cur->_next;
-			delete del;
-		}
-	}
-
-	List& operator=(List l);
-	void Show();
-	void PushBack(Datatype data);
-	Datatype PopBack();
-	void PushFront(Datatype data);
-	Datatype PopFront();
-
-private:
-	Node* _head;
-	Node* _tail;
+public:
+	Node* _ptr;
 };
 
 
-void List:: Show()
-{
-	Node* cur = _head;
-	while (cur)
-	{
-		cout << cur->_data << " ";
-		cur = cur->_next;
-	}
-	cout << endl;
-}
+//输出运算符的重载要写在类的外面
+//template<class T>
+//ostream& operator<<(ostream& stream, Iterator<T> i)
+//{
+//	stream << i->_ptr->_data;
+//	return stream;
+//}
 
-void List:: PushBack(Datatype data)
-{
-	if (NULL == _head)
-	{
-		_head = new Node(data);
-		_tail = _head;
-	}
-	/*else if (NULL == _head->_next)    一开始为了下面popback的需要，我本来想让tail指针指向倒数第二个指针，
-	                                    从而减少popback中定义一个cur指针用来循环，但是popback之后
-                                         tail指针还是无法回到前一个的，所以这种方式错误	                                   
-	{
-		_head->_next = new Node(data);
-		_tail = _head;
-	}*/
-	else
-	{
-		_tail->_next = new Node(data);
-		_tail = _tail->_next;
-	}
-}
 
-Datatype List::PopBack()
+//链表
+template<class T>
+class List
 {
-	Node* cur = _head;
-
-	if (NULL == _head)   //指针为空
-		return 0;
-	else if (NULL == _head->_next)  //指针只有一个元素
+	
+	typedef ListNode<T> Node;
+	
+public:
+	typedef __iterator<T, T&, T*> Iterator;   //这里我们深入理解模板是什么东西，模板之前一直脑子里面有一个想法就是，模板会
+			//自动根据你的使用推演出来一些类型啥的，但是，我在写的时候更多的发现模板显示的调用的机会会比较多一点
+			//就比如这个地方，我们是显式的调用模板了，而且很多时候，写只要是模板类的地方，
+	List()
+		:_head(GetNode(0))
 	{
-		int num = _head->_data;
-		delete _head;
-		_head = _tail = NULL;
-		return num;
+		_head->_next = _head;
+		_head->_prev = _head;
 	}
-	else
+
+public:
+	Node* GetNode(T data)
 	{
-		int num;
-		while (cur->_next->_next)
+		return new ListNode<T>(data);    //这里注意，我开始的时候没有加上<T>的时候，编译通不过，这就是因为ListNode在定义的时候
+				//就定义成了一个模板类，所以在出现ListNode的时候，就出现<T>，他们是一个整体
+				//同时应该注意的是我们在使用其他的模板类的时候也应该注意合适的地方加上<T>
+	}
+
+	void PushBack(T data)
+	{
+		Node* tmp = GetNode(data);
+		Node* next = _head;
+		Node* prev = _head->_prev;
+		tmp->_next = next;
+		tmp->_prev = prev;
+		next->_prev = tmp;
+		prev->_next = tmp;
+		/*tmp->_next = _head;
+		tmp->_prev = _head->_prev;*/
+	}
+
+	void Show()
+	{
+		Node* it = _head->_next;
+		while (it != _head)
 		{
-			cur = cur->_next;
+			cout << it->_data << " ";
+			it = it->_next;
 		}
-		_tail = cur;
-		num = cur->_next->_data;
-		delete cur->_next;
-		cur->_next = NULL;
-		return num;
+		cout << endl;
 	}
 
-}
+	Iterator Begin()   //我这里一开始返回的是一个Node*的一个指针，但是在外面使用的时候感觉是不符合要求的，因为我的Iterator是
+				//一个类型，所以我们这返回的应该是一个Iterator类型的一个对象
+	{
+		return Iterator(_head->_next);
+	}
+protected:
+	Node* _head;
+};
 
-void List:: PushFront(Datatype data)
-{
-	if (NULL == _head)
-	{
-		_head = new Node(data);
-		_tail = _head;
-	}
-	else
-	{
-		Node* cur = _head;
-		_head = new Node(data);
-		_head->_next = cur;
-	}
-}
 
-Datatype List:: PopFront()
-{
-	if (NULL == _head)    //没有节点
-	{
-		return 0;
-	}
-	else if (NULL == _head->_next)    //只有一个节点
-	{
-		int num = _head->_data;        //这里和Popback中只有一个节点的处理是一样的，代码直接拿下来
-		delete _head;
-		_head = _tail = NULL;
-		return num;
-	}
-	else
-	{
-		int num = _head->_data;
-		Node*del = _head;
-		_head = _head->_next;
-		delete del;
-		return num;
-	}
-}
 
-List& List::operator=(List l)
-{
-	swap(_head,l._head);            //现代写法
-	swap(_tail, l._tail);
-	return *this;
-}
-
-void Test()
-{
-	List l1;
-	l1.PushBack(1);
-	l1.PushBack(2);
-	l1.PushBack(3);
-	l1.PushBack(4);
-	l1.Show();
-	List l2(l1);
-	l2.Show();
-	List l3;
-	l3.Show();
-	l3 = l2;
-	l3.Show();
-	/*cout << l1.PopBack() << endl;
-	l1.Show();
-	l1.PopBack();
-	l1.PopBack();
-	l1.Show();
-	l1.PopBack();
-	l1.Show();
-	l1.PushFront(9);
-	l1.PushFront(8);
-	l1.Show();
-	cout << l1.PopFront() << endl;
-	l1.Show();*/
-}
