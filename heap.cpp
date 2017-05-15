ec1cfdc34c80f3fc81efde359629d89e23e44de5
NeilZhy <NeilZhy@163.com>
2017-05-15 20:06:41 +0800

    堆cpp

---
+++ heap.cpp
@@ -0,0 +1,14 @@
+#include"heap.h"
+
+void TestHeap()
+{
+	int arr[] = { 1, 5, 3, 8, 4, 6, 9, 5 };
+	size_t size = sizeof(arr) / sizeof(arr[0]);   //这里注意的一个点就是，要除以sizeof(arr[0])
+	Heap<int> hp(arr, size);
+}
+
+int main()
+{
+	TestHeap();
+	return 0;
+}
