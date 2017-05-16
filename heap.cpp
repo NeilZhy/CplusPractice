bd277c0cda57913d6c0cbee24457a084f2c049b1
NeilZhy <NeilZhy@163.com>
2017-05-16 10:09:47 +0800

    堆cpp

--- heap.cpp
+++ heap.cpp
@@ -5,6 +5,9 @@ void TestHeap()
 	int arr[] = { 1, 5, 3, 8, 4, 6, 9, 5 };
 	size_t size = sizeof(arr) / sizeof(arr[0]);   //这里注意的一个点就是，要除以sizeof(arr[0])
 	Heap<int> hp(arr, size);
+	hp.PushBack(10);
+	hp.PopFront();
+	hp.PopFront();
 }
 
 int main()
