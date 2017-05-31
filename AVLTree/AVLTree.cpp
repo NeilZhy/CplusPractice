1430752fb55d24eb1cc5084a5a96a09ce356e06e
NeilZhy <NeilZhy@163.com>
2017-05-31 20:14:25 +0800

    平衡二叉树

--- AVLTree/AVLTree.cpp
+++ AVLTree/AVLTree.cpp
@@ -4,13 +4,34 @@ using namespace std;
 
 void TestAVLTree()
 {
+	/*int arr[] = { 4, 2, 6, 1, 3, 5, 15, 7, 16, 14 };*/
+	int arr[] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
+	int len = sizeof(arr) / sizeof(arr[0]);
 	AVLTree<int> tree;
-	tree.Insert(5);
-	tree.Insert(8);
-	tree.Insert(1);
-	tree.Insert(7);
-	tree.Insert(6);
-	tree.Insert(0);
+	for (int i = 0; i < len; i++)
+	{
+		tree.Insert(arr[i]);
+	}
+
+	//这组数据还没有测试过，回去测试吧
+
+
+
+
+
+	//tree.Insert(5);
+	//tree.Insert(8);
+	//tree.Insert(1);
+	//tree.Insert(7);
+	//tree.Insert(9);
+	//tree.Insert(-5);
+	///*tree.Insert(2);
+	//tree.Insert(3);
+	//tree.Insert(4);*/
+	//tree.Insert(-4);
+	tree.IsBanlance();
+
+
 }
 
 
--- AVLTree/AVLTree.h
+++ AVLTree/AVLTree.h
@@ -71,50 +71,48 @@ public:
 			{
 				return false;
 			}
-
-
-			
 		}
+
 		cur = insert;
-		while (parent)  
+		while (parent)
 		{
 			//首先更新当前的，右++，左--
 			if (parent->_left == cur)
 			{
 				parent->_bf--;
-				
+
 			}
 			if (parent->_right == cur)
 			{
 				parent->_bf++;
-				
+
 			}
 
-			
+
 
 			//旋转
 			if (parent->_bf == 2 && parent->_right->_bf == 1)
 			{
-			
+
 				RotaL(parent);
 
 			}
 			if (parent->_bf == -2 && parent->_left->_bf == -1)
 			{
-				
+
 				RotaR(parent);
 			}
 			if (parent->_bf == 2 && parent->_right->_bf == -1)
 			{
-				
+				//先右旋后左旋
 				RotaR(parent->_right);
 				RotaL(parent);
 			}
 			if (parent->_bf == -2 && parent->_left->_bf == 1)
 			{
-			
-				RotaR(parent->_left);
-				RotaL(parent);
+				//先左旋后右旋
+				RotaL(parent->_left);
+				RotaR(parent);
 			}
 
 			if (0 == parent->_bf)  //当我们的其中一个父节点的bf为的时候，就不需要再调整了
@@ -123,43 +121,89 @@ public:
 			}
 			cur = parent;
 			parent = parent->_parent;
-			
+
 		}
 		return true;
 	}
 
+	bool IsBanlance()
+	{
+		int num = 0;
+		return _IsBanlance(_root,num);
+	}
+
 private:
-	void RotaL(Node* parent)   //左单
+	//我们想判断一个AVLTree是不是符合 我们的要求,这里我们的判断方法 是通过计算树（还有子树）的高度，比较差距，然后得到
+	//高度差，这样进行比较
+	bool _IsBanlance(Node* root,int& length)
+	{
+		//结束条件
+		if (NULL == root)
+		{
+			length = 0;
+			return true;
+		}
+
+		//子问题
+		_IsBanlance(root->_left, length);
+		int left = length;
+		_IsBanlance(root->_right, length);
+		int right = length;
+		if (abs(left - right) > 1)
+		{
+			cout << "AVL  false" << root->_k << endl;
+			return false;
+		}	
+		if (left >= right)
+		{
+			length = left;
+		}
+		else
+		{
+			length = right;
+		}
+		//原子问题处理
+		//在子问题中，我们想，递归的时候每次往上一层递归的时候保存高度，递归往上的时候就随时加1
+		length++;
+		cout << "AVL  true" << root->_k << endl;
+		return true;
+	}
+
+
+	void RotaL(Node* parent)   //右旋
 	{
 		Node* pprent = parent->_parent;
-		Node* subL = parent->_left;
-		Node* subLR = subL->_right;
-		parent->_right = subLR;
-		subLR->_parent = parent;
-		subL->_right = parent;
-		parent->_parent = subL;
+		Node* subR = parent->_right;
+		Node* subRL = subR->_left;
+		parent->_right = subRL;
+		if (NULL != subRL)    //这个时候subLR可能为NULL，这个时候他是没有父亲的
+		{
+			subRL->_parent = parent;
+		}
+		subR->_left = parent;
+		parent->_parent = subR;
 		if (pprent->_left == parent)
 		{
-			pprent->_left = subL;
-			subL->_parent = pprent;
+			pprent->_left = subR;
+			subR->_parent = pprent;
 		}
 		if (pprent->_right == parent)
 		{
-			pprent->_right = subL;
-			subL->_parent = pprent;
+			pprent->_right = subR;
+			subR->_parent = pprent;
 		}
 		//如果是根节点
-		if (pprent == _root)
+		if (pprent == NULL)
 		{
-			_root = subL;
+			_root = subR;
 		}
 
 		//调整平衡因子，这里把平衡因子都调整为0
-		subL->_bf = 0;
+		subR->_bf = 0;
 		parent->_bf = 0;
 	}
 
-	void RotaR(Node* parent)   //左单
+	void RotaR(Node* parent)   //右旋
 	{
 		Node* pprent = parent->_parent;
 		Node* subL = parent->_left;
@@ -171,26 +215,34 @@ private:
 		}	
 		subL->_right = parent;
 		parent->_parent = subL;
-		if (pprent->_right == parent)
-		{
-			pprent->_right = subL;
-			subL->_parent = pprent;
-		}
-		if (pprent->_left == parent)
-		{
-			pprent->_left = subL;
-			subL->_parent = pprent;
-		}
+		//当pprent为NULL的时候，我们下面的这个就不能通过
+		
 		//如果是根节点
 		if (pprent == NULL)
 		{
 			_root = subL;
 		}
+		else
+		{
+			if (pprent->_right == parent)
+			{
+				pprent->_right = subL;
+				subL->_parent = pprent;
+			}
+			if (pprent->_left == parent)
+			{
+				pprent->_left = subL;
+				subL->_parent = pprent;
+			}
+		}
+	
 
 		//调整平衡因子，这里把平衡因子都调整为0
 		subL->_bf = 0;
 		parent->_bf = 0;
 	}
+
+
 
 
 private:
